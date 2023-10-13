1a33feea43fa23c37fcf224353b24da8ad3d37f6
Juan Insuasty <juan.insuasty@epik.com.co>
2023-10-12 22:06:18 -0500

    build: Configuración para iOS

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -6,12 +8,14 @@ import 'package:wellbeing_checkup/src/app.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await Firebase.initializeApp(
-      options: const FirebaseOptions(
-          apiKey: "AIzaSyCjI2-j4hPvjVkDu9Y5lnsrcAZG9EZtX-A",
-          appId: "1:687661812126:android:cb97a83d618a3fac51ee8f",
-          messagingSenderId: "687661812126",
-          projectId: "wellbeingcheckup"));
+  Platform.isAndroid
+      ? await Firebase.initializeApp(
+          options: const FirebaseOptions(
+              apiKey: "AIzaSyCjI2-j4hPvjVkDu9Y5lnsrcAZG9EZtX-A",
+              appId: "1:687661812126:android:cb97a83d618a3fac51ee8f",
+              messagingSenderId: "687661812126",
+              projectId: "wellbeingcheckup"))
+      : await Firebase.initializeApp();
 
   runApp(const ProviderScope(child: App()));
 }
